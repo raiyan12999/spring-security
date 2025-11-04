6190268af62b4d6a8d9b9473b93c8c635664c65d
raiyan12999 <raiyanbinatik.m29@gmail.com>
2025-11-04 02:34:03 -0800

    testing 2

--- src/main/java/com/codingshuttle/youtube/hospitalManagement/controller/AdminController.java
+++ src/main/java/com/codingshuttle/youtube/hospitalManagement/controller/AdminController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 // Checking ..
+//Checking two ..
 @RestController
 @RequestMapping("/admin")
 @RequiredArgsConstructor
